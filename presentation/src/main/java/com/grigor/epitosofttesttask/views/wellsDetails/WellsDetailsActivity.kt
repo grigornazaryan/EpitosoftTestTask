e3246e1a79375148fd037408acafa05c5d2ed7ef
Grigor Nazaryan <grigor.nazaryan2@icloud.com>
2022-05-17 21:22:49 +0400

    second commit

--- presentation/src/main/java/com/grigor/epitosofttesttask/views/wellsDetails/WellsDetailsActivity.kt
+++ presentation/src/main/java/com/grigor/epitosofttesttask/views/wellsDetails/WellsDetailsActivity.kt
@@ -1,11 +1,13 @@
 package com.grigor.epitosofttesttask.views.wellsDetails
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.view.LayoutInflater
 import com.grigor.domain.entities.constants.BundleConstants
 import com.grigor.domain.entities.models.dataModels.WellSitesDataModel
 import com.grigor.epitosofttesttask.baseFeatures.BaseActivity
 import com.grigor.epitosofttesttask.databinding.ActivityWellsDetailsBinding
+import com.grigor.epitosofttesttask.extencions.convertGeoHash
 
 class WellsDetailsActivity : BaseActivity<ActivityWellsDetailsBinding>() {
 
@@ -19,11 +21,14 @@ class WellsDetailsActivity : BaseActivity<ActivityWellsDetailsBinding>() {
         model = intent.getParcelableExtra(BundleConstants.MODEL_TO_DETAILS_ACTIVITY)
     }
 
+    @SuppressLint("SetTextI18n")
     override fun initViews() {
         mBinding.includedCard.apply {
             name.text=model?.name
             apiName.text=model?.api
-            location.text=model?.location
+            convertGeoHash(model?.location)?.let {
+            location.text="lat ${it.latitude} lon ${it.longitude}"
+            }
         }
     }
 }
